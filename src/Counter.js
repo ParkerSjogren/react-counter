1143801980624c0307f002a07ce35d544fbd29f2
ParkerSjogren <sjogrenp@telus.net>
2021-04-30 18:40:11 -0600

    first react-counter commit

---
+++ src/Counter.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react'
+
+export default function StatedBasedComponent() {
+    const [count,setCount] = useState(0)
+
+    return <div><p>You clicked {count} times</p> <button onClick={() => setCount(count + 1)}>Click Me</button></div>
+}
